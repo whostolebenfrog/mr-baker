a8ca6a7bcb0d2adb961eb4a40a94e9d4ea3e8079
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-09-09 12:05:45 +0100

    Fix packer output parser

--- src/ditto/packer.clj
+++ src/ditto/packer.clj
@@ -5,24 +5,28 @@
 
 (conch/programs packer)
 
+;; TODO - check for the word Error: in here somewhere, if appears then fail
 (defn packer-build
   "Builds the template and returns the ami-id from the output"
   [template-path]
   (let [packer-out (packer "build" template-path)]
     (prn packer-out)
     (->> packer-out
-         (re-matches #"^(?!==> amazon-ebs).*$")
+         (re-matches #"(?s).*AMIs were created.*(ami-[A-Za-z0-9]+).*$")
          (second))))
 
-;; TODO - checking the validate doesn't work! needs to check exit code
+;; TODO - needs to return the full response
 (defn build
   "Build the provided template and respond with the created ami id"
   [template]
   (let [file-name (str "/tmp/" (java.util.UUID/randomUUID))]
     (try
       (spit file-name template)
-      (let [{:keys [exit-code stout]} (packer "validate" file-name {:verbose true})]
-        (prn stout)
+      (let [{:keys [exit-code stdout] :as xx} (packer "validate" file-name {:verbose true})]
+        (prn "printing validation")
+        (prn xx)
+        (prn @exit-code)
+        (prn stdout)
         (if-not (pos? @exit-code)
           {:status 200
            :body (json/generate-string {:status 200 :message (packer-build file-name)})}
