70c7ae8ff3d85aebc58d57af02b1ac8aeb42049f
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-09-11 17:30:08 +0100

    Bake service ami impl

--- src/ditto/bake_service_ami.clj
+++ src/ditto/bake_service_ami.clj
@@ -8,6 +8,10 @@
              [core :as time-core]
              [format :as time-format]]))
 
+;; TODO - presumably we are going to have to start an instance for this baking task
+   ;; when we provision the new base-ami
+
+;; TODO _ put the service name in here!
 (defn service-ami-name
   "Returns the ami name for the service with date/time now"
   [service-name]
@@ -15,7 +19,6 @@
                               (time-format/formatter "YYYY-MM-dd_HH-mm-ss")
                               (time-core/now))))
 
-;; TODO
 (defn motd
   "Set up the message of the day"
   [service-name service-version]
@@ -28,7 +31,7 @@
   "Generates a new ami template for the servivce"
   [service-name service-version]
   {:builders [{:access_key (env :service-aws-access-key)
-               :ami_name (service-ami-name service-name)
+               :ami_name (service-ami-name service-name service-version)
                :iam_instance_profile "baking"
                :instance_type "t1.micro"
                :region "eu-west-1"
@@ -39,7 +42,7 @@
                :ssh_timeout "5m"
                :ssh_username "nokia"
                :subnet_id "subnet-bdc08fd5"
-               :type "amazon-chroot"
+               :type "amazon-ebs"
                :vpc_id "vpc-7bc88713"}]
    :provisioners [(motd service-name service-version)]})
 
