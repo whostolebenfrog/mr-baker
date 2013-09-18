98ab5eabe9eafeca005f0f6bdb8903007162c2bb
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-09-18 11:46:37 +0100

    Return instance name or nil rather than empty seq

--- src/ditto/aws.clj
+++ src/ditto/aws.clj
@@ -1,14 +1,15 @@
 (ns ditto.aws
   (:require [me.raynes.conch :as conch]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            ))
 
 (conch/programs aws)
 
-(defn get-owned-images-by-name
+(defn owned-images-by-name
   "Returns a list of images owned by the current account and filtered by the supplied name.
    Accepts * as a wild card.
 
-   Returns an array of matching images or [].
+   Returns an array of matching images or nil.
 
    Images are returned sorted alphabetically by name."
   [name]
@@ -17,15 +18,17 @@
                     "--owner" "self"
                     "--filters" (str "Name=name,Values=" name))
                (json/parse-string true)
-               :Images)))
+               :Images
+               seq)))
 
-(defn get-private-images-by-name
+(defn private-images-by-name
   "Returns a list of private images that match the supplied name.
    Accepts * as a wild card.
 
-   Returns an array of matching images or []"
+   Returns an array of matching images or nil"
   [name]
   (-> (aws "ec2" "describe-images"
            "--filters" (str "Name=name,Values=" name) " Name=is-public,Values=false")
       (json/parse-string true)
-      :Images))
+      :Images
+      seq))
