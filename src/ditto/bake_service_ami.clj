4fb3d54f7150f904d962180ba03c9d5a7bb5d5b3
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-09-19 16:46:00 +0100

    Bake the service ami

--- src/ditto/bake_service_ami.clj
+++ src/ditto/bake_service_ami.clj
@@ -10,10 +10,12 @@
 
 (defn service-ami-name
   "Returns the ami name for the service with date/time now"
-  [service-name]
-  (str "ent-" service-name "-" (time-format/unparse
-                                (time-format/formatter "YYYY-MM-dd_HH-mm-ss")
-                                (time-core/now))))
+  [service-name service-version]
+  (str "ent-" service-name "-"
+       service-version "-"
+       (time-format/unparse
+        (time-format/formatter "YYYY-MM-dd_HH-mm-ss")
+        (time-core/now))))
 
 (defn motd
   "Set up the message of the day"
@@ -23,7 +25,43 @@
                  (time-format/unparse (time-format/formatters :date-time-no-ms) (time-core/now)))
          (format "echo -e \"\\nService: %s %s\\n\" >> /etc/motd" service-name service-version)))
 
-;; TODO - update this to handle ebs / instance style
+;; TODO - this is going to be replaced with a call to get this from tyranitar
+(defn service-rpm
+  "Install the service rpm on to the machine"
+  [service-name service-version]
+  (let [rpm-name (format "%s-%s.noarch.rpm" service-name service-version)]
+    (shell (str "wget http://yumrepo.brislabs.com/ovimusic/" rpm-name)
+           (str "yum -y install " rpm-name)
+           (str "rm -fv " rpm-name))))
+
+(defn props-path
+  "Returnst the path of the properties file"
+  [service-name environment]
+  (str "/opt/service-props/" service-name "/" environment ".properties"))
+
+;; TODO - will go away when we make things more consistent
+(defn trim-number
+  [service-name]
+  "Name without the version number"
+  (re-find #".+[^0-9]" service-name))
+
+;; TODO - this is going to be replaced with a call to get this from tyranitar
+(defn mv-service-properties
+  "Copy over the service properties"
+  [service-name]
+  (let [service-name (trim-number service-name)]
+   (shell (str "mkdir -p /usr/local/" service-name "/etc")
+          (str "mv /tmp/dev.properties /usr/local/" service-name "/etc/dev.properties")
+          (str "mv /tmp/prod.properties /usr/local/" service-name "/etc/prod.properties")
+          (str "chkconfig " service-name " off"))))
+
+(defn upload-service-properties
+  [service-name environment]
+  "Upload the properties files"
+  {:type "file"
+   :source (props-path service-name environment)
+   :destination (str "/tmp/" environment ".properties")})
+
 (defn service-template
   "Generates a new ami template for the servivce"
   [service-name service-version]
@@ -35,13 +73,17 @@
                :secret_key (env :service-aws-secret-key)
                :security_group_id "sg-c453b4ab"
                :source_ami (base/entertainment-base-ami-id :ebs)
-               :temporary_key_pair_name "nokia-{{uuid}}"
+               :temporary_key_pair_name "nokia2-{{uuid}}"
                :ssh_timeout "5m"
-               :ssh_username "nokia"
+               :ssh_username "nokia2"
                :subnet_id "subnet-bdc08fd5"
                :type "amazon-ebs"
                :vpc_id "vpc-7bc88713"}]
-   :provisioners [(motd service-name service-version)]})
+   :provisioners [(motd service-name service-version)
+                  (service-rpm service-name service-version)
+                  (upload-service-properties service-name "dev")
+                  (upload-service-properties service-name "prod")
+                  (mv-service-properties service-name)]})
 
 (defn create-service-ami
   "Creates a new ami for the supplied service and vesion"
