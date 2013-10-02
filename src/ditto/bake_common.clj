6e15e5823fd7926d9a86b5a4ec30a821b0aebe1f
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-10-02 16:29:43 +0100

    Fix mangled attempt at programming

--- src/ditto/bake_common.clj
+++ src/ditto/bake_common.clj
@@ -9,10 +9,13 @@
    :inline_shebang "/bin/sh -x"
    :inline cmds})
 
+;; TODO: I really need to get these from environment variables rather than
+;; having them in the project. I don't want my keys in the version people use
+;; following that I also need to revoke my own access keys as it's in the git history
 (defn maybe-with-keys
   "Includes access keys in the map if iam authentication isn't enabled.
    For example, when not running in aws."
   [builder]
   (merge builder (when-not (Boolean/valueOf (env :service-packer-use-iam))
                                  {:access_key (env :service-aws-access-key)
-                                  :secret_key (env :service-)})))
+                                  :secret_key (env :service-aws-secret-key)})))
