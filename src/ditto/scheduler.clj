5ea4496a3c32a71dfccca8b5118b6090c6f7b428
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-10-15 16:35:23 +0100

    Don't refer - use as

--- src/ditto/scheduler.clj
+++ src/ditto/scheduler.clj
@@ -6,34 +6,34 @@
              [asgard :as asgard]
              [public-ami :as public-ami]
              [onix :as onix]
-             [packer :as packer]])
-  (:require [clj-time.core :refer [now day-of-week today-at-midnight days plus]]
-            [clj-time.coerce :refer [to-long]]
+             [packer :as packer]]
+            [clj-time.core :as core-time]
+            [clj-time.coerce :as coerce-time]
             [clj-http.client :as client]
             [clojure.tools.logging :refer [info warn error]]
             [environ.core :refer [env]]
-            [overtone.at-at :refer [mk-pool every scheduled-jobs show-schedule]]))
+            [overtone.at-at :as at-at]))
 
 (def thursday 4)
 (def week-in-ms (* 60 60 24 7 1000))
 (def day-in-ms (* 60 60 24 1000))
 (def half-an-hour-in-ms (* 30 60 1000))
 
-(def scheduler-pool (mk-pool))
+(def scheduler-pool (at-at/mk-pool))
 
 (defn next-day-start
   "Returns the start of the next day after the specified time, where day = 1 for Monday to 7 for Sunday."
   [time day]
-  (let [dow (day-of-week time)]
+  (let [dow (core-time/day-of-week time)]
     (if (< dow day)
-      (plus (today-at-midnight) (days (- day dow)))
-      (plus (today-at-midnight) (days (- (+ day 7) dow))))))
+      (core-time/plus (core-time/today-at-midnight) (core-time/days (- day dow)))
+      (core-time/plus (core-time/today-at-midnight) (core-time/days (- (+ day 7) dow))))))
 
 (defn initial-delay
   []
-  (let [current-time (now)
+  (let [current-time (core-time/now)
         next-thursday-start (next-day-start current-time thursday)]
-    (- (to-long next-thursday-start) (to-long current-time))))
+    (- (coerce-time/to-long next-thursday-start) (coerce-time/to-long current-time))))
 
 (defn output-piped-input-stream
   [stream]
@@ -90,7 +90,7 @@
    of every Thursday, as a new Nokia ami is made available every Wednesday. "
   ([time-ms initial-delay-ms]
      (info "Setting next base and public api bake time, initial-delay:" initial-delay "interval:" time-ms)
-     (every time-ms
+     (at-at/every time-ms
             bake-amis
             scheduler-pool
             :initial-delay initial-delay-ms
@@ -101,7 +101,7 @@
   "Starts a scheduler for a task that deregisters old amis for all services. The most recent 10 are kept
    along with, if different, the currently live ami."
   ([time-ms initial-delay-ms]
-     (every time-ms
+     (at-at/every time-ms
             kill-amis
             scheduler-pool
             :initial-delay initial-delay-ms
@@ -111,4 +111,4 @@
 (defn job-is-scheduled?
   "Returns truthy if the named job is scheduled"
   [name]
-  (= true (some #(= (:desc %) name) (scheduled-jobs scheduler-pool))))
+  (= true (some #(= (:desc %) name) (at-at/scheduled-jobs scheduler-pool))))
