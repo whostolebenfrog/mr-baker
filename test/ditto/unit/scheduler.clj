bde94ae7e603847f3930a238c3fe267549aee87a
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-10-15 17:26:54 +0100

    Add scheduler unit test file

--- test/ditto/unit/scheduler.clj
+++ test/ditto/unit/scheduler.clj
@@ -1,1 +1,13 @@
-(ns ditto.unit.scheduler)
+(ns ditto.unit.scheduler
+  (:require [ditto
+             [scheduler :refer :all]
+             [onix :as onix]]
+            [midje.sweet :refer :all]))
+
+(fact-group :unit
+            (fact "kill-amis kills amis for all onix applications"
+                  (kill-amis) => truthy
+                  (provided
+                   (kill-amis-for-application ..app1..) => nil :times 1
+                   (kill-amis-for-application ..app2..) => nil :times 1
+                   (onix/get-applications) => [..app1.. ..app2..])))
