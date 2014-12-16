77f1668a1b3f81862f465e66f250e5a2ffca8768
Ben Griffiths <benjamin.griffiths@microsoft.com>
2014-12-16 14:58:29 +0000

    lein-release plugin: preparing 0.148 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ditto "0.148-SNAPSHOT"
+(defproject ditto "0.148"
   :description "Mr-Baker the AMI maker."
 
   :dependencies [[amazonica "0.2.28" :exclusions [com.fasterxml.jackson.core/jackson-annotations]]
