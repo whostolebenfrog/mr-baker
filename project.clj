2afd782b44afc5fd783ee8c01690844d5b530975
Ben Griffiths <benjamin.griffiths@microsoft.com>
2015-01-05 15:17:21 +0000

    Bump version to above branch

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ditto "0.152-SNAPSHOT"
+(defproject ditto "0.155-SNAPSHOT"
   :description "Mr-Baker the AMI maker."
 
   :dependencies [[amazonica "0.2.28" :exclusions [com.fasterxml.jackson.core/jackson-annotations]]
