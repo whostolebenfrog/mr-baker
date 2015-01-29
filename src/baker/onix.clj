10c68aaa5a459d213e774d9c51748dceefd532cf
Ben Griffiths <benjamin.griffiths@microsoft.com>
2015-01-29 11:23:20 +0000

    Allow standalone mode by disabling onix

--- src/baker/onix.clj
+++ src/baker/onix.clj
@@ -5,34 +5,48 @@
 (def onix-base-url
   (env :onix-url))
 
+(defn onix-available?
+  []
+  "If we are running in standalone mode without onix then effectively
+   don't perform these checks"
+  (Boolean/valueOf (env :onix-available?)))
+
 (defn service-exists?
   "Returns true if the service is known to onix; otherwise returns false."
   [service-name]
-  (let [app-url (str onix-base-url "/applications/" service-name)
-        response (client/head app-url {:throw-exceptions false})
-        status (:status response)]
-    (= status 200)))
+  (if (onix-available?)
+    (let [app-url (str onix-base-url "/applications/" service-name)
+          response (client/head app-url {:throw-exceptions false})
+          status (:status response)]
+      (= status 200))
+    true))
 
 (defn get-applications
   "Returns the list of all applications in onix"
   []
-  (get-in (client/get (str onix-base-url "/applications")
-                      {:throw-exceptions false :as :json})
-          [:body :applications]))
+  (if (onix-available?)
+    (get-in (client/get (str onix-base-url "/applications")
+                        {:throw-exceptions false :as :json})
+            [:body :applications])
+    []))
 
 (defn service-property
   "Returns the onix property for the supplied name and service"
   [service-name property-name]
-  (get-in (client/get (str onix-base-url "/applications/" service-name)
-                      {:throw-exceptions false :as :json})
-          [:body :metadata (keyword property-name)]))
+  (when (onix-available?)
+    (get-in (client/get (str onix-base-url "/applications/" service-name)
+                        {:throw-exceptions false :as :json})
+            [:body :metadata (keyword property-name)])))
 
 (defn rpm-name
   "Returns any overridden RPM name for the given service, or nil if not defined"
   [service-name]
-  (service-property service-name "rpmName"))
+  (when (onix-available?)
+    (service-property service-name "rpmName")))
 
 (defn shell-commands
   "Returns a list of shell commands to run on the service"
   [service-name]
-  (service-property service-name "customBakeCommands"))
+  (if (onix-available?)
+    (service-property service-name "customBakeCommands")
+    []))
--- test/baker/unit/onix.clj
+++ test/baker/unit/onix.clj
@@ -1,9 +1,15 @@
 (ns baker.unit.onix
   (:require [baker.onix :refer :all]
             [clj-http.client :as client]
+            [environ.core :refer [env]]
             [midje.sweet :refer :all]))
 
-(fact-group :unit
+(fact-group
+ :unit
+
+ (against-background
+  [(onix-available?) => true]
+
   (fact "Onix returns truthy if service exists in onix"
         (service-exists? ..name..) => truthy
         (provided (client/head (contains ..name..) anything) => {:status 200}))
@@ -15,4 +21,4 @@
   (fact "Get all applications returns the list of all applications from onix"
         (get-applications) => [..app1.. ..app2..]
         (provided (client/get (contains "application") anything) =>
-                  {:status 200 :body {:applications [..app1.. ..app2..]}})))
+                  {:status 200 :body {:applications [..app1.. ..app2..]}}))))
