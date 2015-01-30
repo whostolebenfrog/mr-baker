2208ab85a5f07a760bf938c33c6ea2cbf7636a72
Ben Griffiths <benjamin.griffiths@microsoft.com>
2015-01-30 16:47:59 +0000

    Add non packer template

--- src/baker/builders/bake_example_template.clj
+++ src/baker/builders/bake_example_template.clj
@@ -36,7 +36,7 @@
   [name version]
   [(install-rpm name version)])
 
-(defn packer-template
+(defn packer-template-chroot
   "Generates a new ami template for chroot bake of the service"
   [name version source-ami virt-type]
   (let [builder (maybe-with-keys
@@ -49,10 +49,56 @@
     {:builders [builder]
      :provisioners (provisioners name version)}))
 
-;; TODO - we need a non-chroot example as well I think
+(defn packer-template
+  "Generates a new ami template for a standard ebs bake of the service. You may need
+  to create new iam roles for baking depending on the requirements of your template. The
+  example here provides a placeholder for vpc and security groups. If you have no vpc then
+  ommit this entry along with the subnet_id, if your bake requires network access then you will need to provide a
+  security group to allow this. ssh_username assumes amazon-linux in this example. "
+  [name version source-ami virt-type]
+  (let [builder (maybe-with-keys
+                 {:ami_name (service-ami-name name version virt-type)
+                  :iam_instance_profile "baking"
+                  :instance_type (instance-type-for-virt-type virt-type)
+                  :region "eu-west-1"
+                  :run_tags {:name (format "%s AMI Bake" name)
+                             :owner "baker"
+                             :description (format "Temp instance used to bake the %s ent ami" name)}
+                  :tags {:name (format "%s AMI" name)
+                         :service name}
+                  :security_group_id "security-group-id-placeholder"
+                  :source_ami source-ami
+                  :ssh_timeout "5m"
+                  :ssh_username "ec2-user"
+                  :subnet_id "target-subnet-id-placeholder"
+                  :type "amazon-ebs"
+                  :vpc_id "vpc-id-placeholder"})]
+    {:builders [builder]
+     :provisioners (provisioners name version)}))
+
+(defn bake-example-ami-chroot
+  "Bake a new ami for the service name and version based on the latest base ami.
+  If dry-run then only return the packer template, don't run it.
+
+  This bake uses a chroot packer template, this makes the bake faster and avoids
+  starting up new instances to bake the template. However it does mean that baker
+  must be running in aws for it to function on an instance of the same underlying
+  OS as the target. It also doesn't work properly on some operating systems due
+  to some limitations in packer. Amazon linux works very well but some RH based
+  distros will fail without specific boot options."
+  [name version dry-run virt-type]
+  {:pre [#{:para :hvm} virt-type]}
+  (if (not (onix/service-exists? name))
+    (error-response (str "The service '" name "' doesn't exist.") 404)
+    (let [template (packer-template-chroot name version
+                                           (amis/parent-ami virt-type)
+                                           virt-type)]
+      (if dry-run
+        (common/response (json/generate-string template))
+        (common/response (packer/build template name))))))
 
 (defn bake-example-ami
-  "Bake a new ami for the service name and version based on the latest base ent ami.
+  "Bake a new ami for the service name and version based on the latest base ami.
    If dry-run then only return the packer template, don't run it."
   [name version dry-run virt-type]
   {:pre [#{:para :hvm} virt-type]}
